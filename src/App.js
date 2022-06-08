a5931dc4d3cca3c9f4af53cfe4f17bf49ee7ec55
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-06-08 12:01:41 +0200

    change():change the resources upload right

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+const Header=(props)=>{
+  
+  return(
+    <>
+      <h1>{props.course}</h1>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+const Part=(props)=>{
+  
+  return (
+    <>
+    <p> {props.part1} {props.exercises1}</p>
+    <p> {props.part2} {props.exercises1}</p>
+    <p> {props.part3} {props.exercises3}</p>
+    </>
+  )
+}
+const Content=()=>{
+  const part1="Fundamentals of React"
+  const part2="Using props to pass data"
+  const part3="State of a component"
+  const exercises1= 10
+  const exercises2=7
+  const exercises3=14
+  return (
+    <div>
+    <Part part1={part1} exercises1={exercises1}/>
+    <Part part2={part2} exercises2={exercises2}/>
+    <Part part3={part3} exercises3={exercises3}/>
+  
+  </div>
+  )
+}
+
+const App=()=>{
+  const course="Half Stack application development"
+  
+  return (
+    <>
+      <Header course={course}/>
+      <Content />
+    </>
+  )
+}
+
+export default App
