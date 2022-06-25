3aa6eb96d2627c42469f5b3612ebe14acfc61c4d
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-06-25 14:54:11 +0200

    feat():useState and eventhandler

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-const Header=(props)=>{
-  
+import { useState } from "react"
+
+const Hello=({name,age})=>{
+  const bornYear=()=>{
+    return new Date().getFullYear() - age
+  }
   return(
-    <>
-      <h1>{props.course}</h1>
-
-    </>
-  )
-}
-
-const Part=(props)=>{
-  
-  return (
-    <>
-    <p> {props.part1} {props.exercises1}</p>
-    <p> {props.part2} {props.exercises1}</p>
-    <p> {props.part3} {props.exercises3}</p>
-    </>
-  )
-}
-const Content=()=>{
-  const part1="Fundamentals of React"
-  const part2="Using props to pass data"
-  const part3="State of a component"
-  const exercises1= 10
-  const exercises2=7
-  const exercises3=14
-  return (
     <div>
-    <Part part1={part1} exercises1={exercises1}/>
-    <Part part2={part2} exercises2={exercises2}/>
-    <Part part3={part3} exercises3={exercises3}/>
-  
-  </div>
+      <p> Hello {name}, you are {age} and you were probably born in {bornYear()}</p>
+    </div>
   )
 }
+const Display=({counter})=>{
+  return (
+    <>{counter}</>
+  )
+}
+const Button=({onClick,text})=><button onClick={onClick}>{text}</button>
+   
 
 const App=()=>{
-  const course="Half Stack application development"
-  
-  return (
-    <>
-      <Header course={course}/>
-      <Content />
-    </>
-  )
-}
+  const name="Peter"
+  const age=10
+  const [counter,setCounter]=useState(0)
+  const increseBy10= ()=> setCounter(counter +10)
+  const setToZero=()=> setCounter(0)
+  const decreasedBy10=()=> setCounter(counter -10)
 
+  return(
+    <div>
+    <h1>Greetings</h1>
+    <Hello name="Maya" age={20+10}/>
+    <Hello name={name} age={age}/>
+    <Display counter={counter}/>
+    <button onClick={increseBy10} text="plus"></button>
+    <button onClick={setToZero} text="Zero"></button>
+    <button onClick={decreasedBy10} text="minus10"></button>
+  </div>
+  )
+  
+ 
+  
+}
 export default App
